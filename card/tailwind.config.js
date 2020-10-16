c4e16726aec4ce50570a448c9dea6e372006a5c6
Mahdi Naderian <naderian.mahdi@gmail.com>
2020-10-16 15:53:48 +0330

    restructured files.

---
+++ card/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: {
+    enabled: true,
+    content: ['*.html']
+  },
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
