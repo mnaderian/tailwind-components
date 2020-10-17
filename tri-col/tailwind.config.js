173ad5627b5f862c2ec655b50b1f74bb5c444fa1
Mahdi Naderian <naderian.mahdi@gmail.com>
2020-10-17 16:49:19 +0330

    added tri-col component.

---
+++ tri-col/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: true,
+    content: ['*.html']
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
