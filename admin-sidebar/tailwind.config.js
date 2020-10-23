5d80f327722daf3a9fcb9efbf7f3823920668ccf
Mahdi Naderian <naderian.mahdi@gmail.com>
2020-10-23 11:19:34 +0330

    updated admin-sidebar tailwind configs.

--- admin-sidebar/tailwind.config.js
+++ admin-sidebar/tailwind.config.js
@@ -14,15 +14,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'mn-gray-100': '#F4F5F7',
-        'mn-gray-200': '#E4E7EB',
-        'mn-gray-300': '#D2D6DB',
-        'mn-gray-400': '#9EA6B2',
-        'mn-gray-500': '#374151',
-        'mn-gray-600': '#252F3F',
-        'mn-gray-700': '#161D2E',
-      }
-    }
+        'mn-gray': {
+          '100': '#F4F5F7',
+          '200': '#E4E7EB',
+          '300': '#D2D6DB',
+          '400': '#9EA6B2',
+          '500': '#374151',
+          '600': '#252F3F',
+          '700': '#161D2E',
+        },
+      },
+    },
   },
   variants: {
     backgroundColor: ['responsive', 'hover', 'group-hover'],
