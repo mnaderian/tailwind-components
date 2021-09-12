0c95c6720696d3d44ff4138b3155efa277aae661
Mahdi Naderian <naderian.mahdi@gmail.com>
2021-09-12 13:02:47 +0430

    Upgraded game-card-portrait component

--- game-card-portrait/tailwind.config.js
+++ game-card-portrait/tailwind.config.js
@@ -1,12 +1,6 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: {
-    enabled: true,
-    content: ['*.html']
-  },
+  mode: 'jit',
+  purge: ['*.html'],
   theme: {
     extend: {
       colors: {
