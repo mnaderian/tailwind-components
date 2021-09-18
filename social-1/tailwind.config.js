9bd5787d04b14ddccc395076c2cf13e3330d739f
Mahdi Naderian <naderian.mahdi@gmail.com>
2021-09-18 18:10:55 +0430

    New Component: social-1

---
+++ social-1/tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  mode: 'jit',
+  purge: ['*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'sky': colors.sky,
+      },
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif'],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
